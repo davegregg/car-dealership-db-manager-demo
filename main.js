434c711a59b48db1c5ec0e6af59b98d27e143a4d
davegregg <davegregg@gmail.com>
2021-09-03 09:54:58 -0400

    starter code

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+    A car object looks like:
+    {
+        "id": 1,
+        "make": "Volkswagen",
+        "model": "Jetta",
+        "modelYear": 2011,
+        "color": "Purple",
+        "numberOfDoors": 2,
+        "price": "$5325.89",
+        "vin": "1G4CW54K844908680"
+    }
+*/
